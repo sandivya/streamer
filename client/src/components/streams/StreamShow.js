05552232fdbf08aa16600093488ef71ed3cb6842
Sandivya Saxena <saxenasandivya@gmail.com>
2020-12-05 11:40:30 +0530

    Setup Streaming Server

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,15 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flvjs from "flv.js";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
+  componentWillUnmount = () => {
+    this.player.destroy();
+  };
+
   componentDidMount = () => {
     this.props.fetchStream(this.props.match.params.id);
+    this.player = flvjs.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.props.match.params.id}`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   };
 
   render() {
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{this.props.stream?.title}</h1>
         <h5>{this.props.stream?.description}</h5>
       </div>
